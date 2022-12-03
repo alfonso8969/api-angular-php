4215d19b92afde601f5711a3e61932976bcc1c84
alf8969 <alfonso8969@hotmail.es>
2022-12-03 22:28:03 +0100

    Se crean los ficheros para obtener los datos de los usuarios.

---
+++ listUsersInha.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Profile query.
+ * php version 8.1.10
+ *  
+ * @category Config
+ * @package  Server
+ * @author   Author <alfonsoj.gonzalez@alfonsogonz.es>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @version  GIT: @1.0.0
+ * @link     https://github.com/alfonso8969/api-angular-php.git
+ */
+
+include_once "./classes/class.Database.php";
+include_once "./classes/utils.php";
+
+$sql = "CALL ListUsersInha";
+$users = Database::get_array($sql);
+
+$users = Utils::utf8Converter($users);
+
+$response = json_encode(array('data' => $users));
+
+echo "$response";
