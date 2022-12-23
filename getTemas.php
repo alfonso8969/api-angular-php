80a54399aefd56b241cd2aa768df0fccb580c7cb
alf8969 <alfonso8969@hotmail.es>
2022-12-23 01:50:18 +0100

    Se añade ticket support

---
+++ getTemas.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Profile query.
+ * php version 8.1.10
+ *  
+ * @category Config
+ * @package  Server
+ * @author   Author <alfonsoj.gonzalez@alfonsogonz.es>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @version  GIT: @1.0.0
+ * @link     https://github.com/alfonso8969/api-angular-php.git
+ */
+
+include_once "./classes/class.Database.php";
+include_once "./classes/utils.php";
+
+
+$sql = "SELECT * FROM empresas_temas;";
+
+$fields = Database::get_array($sql);
+
+$fields = Utils::utf8Converter($fields);
+
+echo json_encode($fields);
---
+++ saveTicket.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Profile query.
+ * php version 8.1.10
+ *  
+ * @category Config
+ * @package  Server
+ * @author   Author <alfonsoj.gonzalez@alfonsogonz.es>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @version  GIT: @1.0.0
+ * @link     https://github.com/alfonso8969/api-angular-php.git
+ */
+
+include_once "./classes/class.Database.php";
+include_once "./classes/utils.php";
+
+$data = json_decode(file_get_contents("php://input"));
+$sql = "";
+
+if (!isset($data)) {
+    echo "error";
+}
+$ticket = $data->ticket;
+
+$id_ticket = $ticket->id_ticket;
+$user_id = $ticket->user_id;
+$campo = $ticket->campo;
+$message = $ticket->message;
+$phpdate = strtotime($ticket->fecha);
+$mysqldate = date('Y-m-d H:i:s', $phpdate);
+$respondido = $ticket->respondido;
+
+$db = new Database();
+$conn = $db->getConnection();
+$stmt = $conn->prepare("INSERT INTO empresas_ticket (
+                        user_id,
+                        campo,
+                        message,
+                        fecha,
+                        respondido) 
+                        VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("isssi", $user_id, $campo, $message, $mysqldate, $respondido);
+                    
+$ticket = $stmt->execute();
+if ($ticket) {
+    echo 1;
+} else {
+    echo 0;
+}
+
+
+$stmt->close();
+$conn->close();
